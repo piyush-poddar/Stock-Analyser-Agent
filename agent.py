33ce2233556ec4d46fb0759eada879d74e450432
Piyush Poddar <contactme.piyush@gmail.com>
2025-03-08 01:59:03 +0530

    Updating Code

--- agent.py
+++ agent.py
@@ -21,6 +21,8 @@ matplotlib.use("Agg")  # To use non-GUI backend
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 if not GEMINI_API_KEY:
     raise ValueError("Please set the GEMINI_API_KEY environment variable")
+else:
+    print("GEMINI API KEY found")
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY", "")) 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.2)
 
